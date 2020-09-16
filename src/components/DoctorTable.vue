4996d2f6f0ad3fc1ff1fdbc9c7529cdbcec615b7
Yohan Sobrinho <yohan_phillip@hotmail.com>
2020-09-15 23:34:03 -0300

    First commit :tada:

---
+++ src/components/DoctorTable.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-mr-md">
+      <q-select
+      filled
+      v-model="model"
+      label="Simple select"
+      :options="computed_items"
+      style="width: 250px"
+      behavior="menu"
+      />
+    </div>
+    <q-table
+      title="Doctors"
+      :data="data"
+      :columns="columns"
+      row-key="name"
+      selection="multiple"
+      :selected.sync="selected"
+      :filter="filter"
+      :pagination.sync="pagination"
+      grid
+      hide-header
+    >
+      <template v-slot:top-right>
+        <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+
+      <template v-slot:item="props">
+        <div
+          class="q-pa-xs col-xs-12 col-sm-6 col-md-4 col-lg-3 grid-style-transition"
+          :style="props.row.available ? 'transform: scale(0.95);' : ''"
+        >
+          <q-card :class="props.row.available ? 'bg-grey-2' : ''">
+            <q-card-section>
+                <q-checkbox dense v-model="props.row.available" :label="props.row.name" />
+                <q-badge class="q-ml-sm"> {{ props.row.available ? 'available' : 'unavailable' }} </q-badge>
+            </q-card-section>
+            <q-separator />
+            <q-list dense>
+              <q-item v-for="col in props.cols.filter(col => col.name !== 'desc')" :key="col.name">
+                <q-item-section>
+                  <q-item-label>{{ col.label }}</q-item-label>
+                </q-item-section>
+                <q-item-section side>
+                  <q-item-label caption>{{ col.value }}</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-card>
+        </div>
+      </template>
+    </q-table>
+  </div>
+</template>
+<script>
+
+const stringOptions = [
+  {
+    label: 'Available',
+    value: true
+  },
+  {
+    label: 'Unavailable',
+    value: false
+  }
+]
+
+export default {
+  data () {
+    return {
+      model: null,
+      filter: '',
+      selected: [],
+      options: stringOptions,
+      pagination: {
+        rowsPerPage: 10
+      },
+      columns: [
+        {
+          name: 'desc',
+          required: true,
+          align: 'left',
+          field: row => row.name,
+          sortable: true
+        },
+        { name: 'upin', label: 'Upin', field: 'upin' },
+        { name: 'zipcode', label: 'Zip code', field: 'zipcode' },
+        { name: 'city', label: 'City', field: 'city' }
+      ],
+      data: [
+        {
+          name: 'John Doe',
+          upin: 202029,
+          zipcode: 92037,
+          city: 'La Jolla',
+          available: false
+        },
+        {
+          name: 'Nick Ramsen',
+          upin: 402910,
+          zipcode: 92037,
+          city: 'La Jolla',
+          available: true
+        },
+        {
+          name: 'Liz Redfield',
+          upin: 910291,
+          zipcode: 92015,
+          city: 'San Diego',
+          available: true
+        },
+        {
+          name: 'Javier Garcia',
+          upin: 202914,
+          zipcode: 92015,
+          city: 'San Diego',
+          available: false
+        },
+        {
+          name: 'Harry Bone',
+          upin: 394840,
+          zipcode: 92015,
+          city: 'San Diego',
+          available: true
+        },
+        {
+          name: 'Kevin Lamkin',
+          upin: 982170,
+          zipcode: 92015,
+          city: 'San Diego',
+          available: true
+        },
+        {
+          name: 'Andrew Stuart',
+          upin: 393920,
+          zipcode: 92037,
+          city: 'La Jolla',
+          available: true
+        },
+        {
+          name: 'Maggie Willians',
+          upin: 655942,
+          zipcode: 92037,
+          city: 'San Diego',
+          available: true
+        }
+      ]
+    }
+  },
+  created: {
+  },
+  computed: {
+    computed_items () {
+      let list = []
+      list = this.data.filter(v => v.avilable === true)
+      return list
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="sass">
+  .grid-style-transition
+    transition: transform .28s, background-color .28s
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <!-- <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    > -->
+    <DoctorTable></DoctorTable>
+  </q-page>
+</template>
+
+<script>
+
+import DoctorTable from 'components/DoctorTable.vue'
+
+export default {
+  components: {
+    DoctorTable
+  },
+  name: 'PageIndex'
+}
+</script>
